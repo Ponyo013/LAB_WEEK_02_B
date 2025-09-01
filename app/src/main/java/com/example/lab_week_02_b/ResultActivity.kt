6eee6ab11c8c77b0bfaf0ee09f5ea900f155b391
Moreno Pradita <morenopradita013@gmail.com>
2025-09-01 10:23:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -0,0 +1,60 @@
+package com.example.lab_week_02_b
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.*
+import android.graphics.*
+
+
+class ResultActivity : AppCompatActivity() {
+    companion object {
+        private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
+    }
+
+    private val backButton: Button
+        get() = findViewById(R.id.back_button)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.background_screen)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        if(intent != null){
+            val colorCode = intent.getStringExtra(COLOR_KEY)
+            val backgroundScreen = findViewById<ConstraintLayout>(R.id.background_screen)
+
+            try {
+                backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+            } catch (ex: IllegalArgumentException) {
+                Intent().let {
+                    errorIntent -> errorIntent.putExtra(ERROR_KEY, true)
+                    setResult(Activity.RESULT_OK, errorIntent)
+                    finish()
+                }
+            }
+
+
+            val resultMessage = findViewById<TextView>(R.id.color_code_result_message)
+            resultMessage.text = getString(R.string.color_code_result_message,
+                colorCode?.uppercase())
+
+        }
+    }
+}
